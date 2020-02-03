29d41ae72e742b0b9c58fe36bd4d7fe9773ae752
ngocAnh1999 <43126165+ngocAnh1999@users.noreply.github.com>
2020-02-03 16:14:51 +0700

    Update find.php

--- Thuvien_sach/find.php
+++ Thuvien_sach/find.php
@@ -1,6 +1,6 @@
 <!DOCTYPE html>
 <?php 
-		$conn = new mysqli('127.0.0.1', "root","password", "dbname");
+		$conn = new mysqli('127.0.0.1', "root","password", "thuvien");
 		mysqli_set_charset($conn, 'UTF8');
 		if ($conn->connect_error) {
 			die("Connection failed: " . $conn->connect_error);
