92dd592e29452b9519552d2ea6ef6362d3db93f5
ngocAnh1999 <43126165+ngocAnh1999@users.noreply.github.com>
2020-02-03 16:15:08 +0700

    Update index.php

--- Thuvien_sach/index.php
+++ Thuvien_sach/index.php
@@ -1,6 +1,6 @@
 <!DOCTYPE html>
 <?php 
-		$conn = new mysqli('127.0.0.1', "root","yourpassword", "yourdbName");
+		$conn = new mysqli('127.0.0.1', "root","yourpassword", "thuvien");
 		mysqli_set_charset($conn, 'UTF8');
 		if ($conn->connect_error) {
 			die("Connection failed: " . $conn->connect_error);
