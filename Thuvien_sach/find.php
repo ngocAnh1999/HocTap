a44fd38f57297523e3017386156b0be6e41ced5c
ngocAnh1999 <43126165+ngocAnh1999@users.noreply.github.com>
2020-02-03 16:12:54 +0700

    sửa password và dbname

--- Thuvien_sach/find.php
+++ Thuvien_sach/find.php
@@ -1,6 +1,6 @@
 <!DOCTYPE html>
 <?php 
-		$conn = new mysqli('127.0.0.1', "root","ngocanh1999", "thuvien");
+		$conn = new mysqli('127.0.0.1', "root","password", "dbname");
 		mysqli_set_charset($conn, 'UTF8');
 		if ($conn->connect_error) {
 			die("Connection failed: " . $conn->connect_error);
