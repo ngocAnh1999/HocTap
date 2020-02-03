d740c0e39888b11b2688d101dba573881633d10d
ngocAnh1999 <43126165+ngocAnh1999@users.noreply.github.com>
2020-02-03 16:13:51 +0700

    sửa password

--- Thongtin_tinh/connect.php
+++ Thongtin_tinh/connect.php
@@ -1,5 +1,5 @@
 <?php
-    $conn = new mysqli("127.0.0.1", "root", "your_password_mysql", "yourDBname");
+    $conn = new mysqli("127.0.0.1", "root", "your_password_mysql", "DanhMuc");
     mysqli_set_charset($conn, 'UTF8');
     if ($conn->connect_error) {
         die("Connection failed: " . $conn->connect_error);
